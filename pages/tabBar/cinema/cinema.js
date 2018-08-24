83209085f3cbb09f74984ee9b3c75f52c77bcc22
zhangzhihao <137596665@qq.com>
2018-08-24 21:59:53 +0800

    新增影院搜索功能

--- pages/tabBar/cinema/cinema.js
+++ pages/tabBar/cinema/cinema.js
@@ -1,66 +1,16 @@
-// pages/tarBar/cinema/cinema.js
+const app = getApp();
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
+  data:{
+    city:'',
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  onLoad(){
+    this.setData({
+      city: app.globalData.city.city_name || '武汉'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  onShow(){
+    if (this.data.city !== app.globalData.city.city_name){
+      city: app.globalData.city.city_name
+    }
   }
 })
