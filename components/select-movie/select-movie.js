932590b5ca501f8d884af74bdcd33d03ea56893f
zhangzhihao <137596665@qq.com>
2018-09-24 18:08:54 +0800

    添加电影滚动选择功能

--- components/select-movie/select-movie.js
+++ components/select-movie/select-movie.js
@@ -4,7 +4,7 @@ Component({
       type: Array,
       value: [],
       observer: function(movies) {
-        if (Array.isArray(movies) && movies.length){
+        if (Array.isArray(movies) && movies.length) {
           this.setData({
             movies
           }, () => {
@@ -13,10 +13,10 @@ Component({
         }
       }
     },
-    defaultSelect:{
-      type:Object,
-      value:null,
-      observer: function (movie){
+    defaultSelect: {
+      type: Object,
+      value: null,
+      observer: function(movie) {
         movie && this.setData({
           movie
         })
@@ -25,21 +25,54 @@ Component({
   },
   data: {
     movies: [],
-    movie:null //选中的电影
+    movie: null, //选中的电影
+    num: 0, //设置滚动条位置
+    size: 0 //电影item的大小（包括margin）
   },
   methods: {
     //选中电影
-   selectMovie(e){
-     const movie = (e && e.currentTarget.dataset.movie) || this.properties.defaultSelect || this.data.movies[0]
-     if (movie === this.data.movie) {
-       return
-     }
-     this.setData({
-       movie
+    selectMovie(e) {
+      const movie = (e && e.currentTarget.dataset.movie) || this.properties.defaultSelect || this.data.movies[0]
+      if (this.data.movie && movie.id === this.data.movie.id) {
+        return
+      }
+      const index = this.data.movies.findIndex(item => item.id === movie.id)
+      if (this.data.size) {
+        this.setData({
+          movie,
+          num: this.data.size * index
+        })
+      } else {
+        this.calcSize().then((size)=>{
+          this.setData({
+            movie,
+            size,
+            num: size * index
+          })
+        })
+      }
+      this.triggerEvent('selectMovie', {
+        movie
+      })
+    },
+    //计算节点大小
+    calcSize() {
+      return new Promise((resolve, reject)=>{
+       const query = wx.createSelectorQuery().in(this)
+       query.select(`#item1`).fields({
+         size: true,
+         computedStyle: ['margin-left']
+       }, function (res) {
+         let size = 0
+         if (res) {
+           size = res.width + parseFloat(res['margin-left'])
+         }
+         resolve(size)
+       }).exec()
      })
-     this.triggerEvent('selectMovie', {
-       movie
-     })
-   }
+    },
+    test(e) {
+      // console.log(e.detail)
+    }
   }
 })
