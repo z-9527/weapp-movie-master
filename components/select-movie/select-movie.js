bc803e79ce6bd9c8128dabbe31cc8b7ad2f1d7cb
zhangzhihao <137596665@qq.com>
2018-09-24 20:38:06 +0800

    完善电影选择组件

--- components/select-movie/select-movie.js
+++ components/select-movie/select-movie.js
@@ -1,3 +1,4 @@
+
 Component({
   properties: {
     movies: {
@@ -26,13 +27,14 @@ Component({
   data: {
     movies: [],
     movie: null, //选中的电影
-    num: 0, //设置滚动条位置
-    size: 0 //电影item的大小（包括margin）
+    scrollLeft: 0, //设置滚动条位置
+    size: 0, //电影item的大小（包括margin）
+    i: 0 //当前电影的索引,下面的函数传递了事件对象后就不能传递其他参数了，所以只能放在data中传递
   },
   methods: {
     //选中电影
     selectMovie(e) {
-      const movie = (e && e.currentTarget.dataset.movie) || this.properties.defaultSelect || this.data.movies[0]
+      const movie = (e && e.currentTarget.dataset.movie) || this.properties.defaultSelect || this.data.movies[this.data.i]
       if (this.data.movie && movie.id === this.data.movie.id) {
         return
       }
@@ -40,14 +42,14 @@ Component({
       if (this.data.size) {
         this.setData({
           movie,
-          num: this.data.size * index
+          scrollLeft: this.data.size * index
         })
       } else {
-        this.calcSize().then((size)=>{
+        this.calcSize().then((size) => {
           this.setData({
             movie,
             size,
-            num: size * index
+            scrollLeft: size * index
           })
         })
       }
@@ -57,22 +59,42 @@ Component({
     },
     //计算节点大小
     calcSize() {
-      return new Promise((resolve, reject)=>{
-       const query = wx.createSelectorQuery().in(this)
-       query.select(`#item1`).fields({
-         size: true,
-         computedStyle: ['margin-left']
-       }, function (res) {
-         let size = 0
-         if (res) {
-           size = res.width + parseFloat(res['margin-left'])
-         }
-         resolve(size)
-       }).exec()
-     })
+      return new Promise((resolve, reject) => {
+        const query = wx.createSelectorQuery().in(this)
+        query.select(`#item1`).fields({
+          size: true,
+          computedStyle: ['margin-left']
+        }, function(res) {
+          let size = 0
+          if (res) {
+            size = res.width + parseFloat(res['margin-left'])
+          }
+          resolve(size)
+        }).exec()
+      })
     },
-    test(e) {
-      // console.log(e.detail)
+    handleScroll(e) {
+    },
+    /**
+     * 本来想用touchend事件来做滚动结束后“选择电影”功能，但是获取scrollOffset有两三秒的延迟，所以就放弃了这种方法
+    handleTouchend(e) {
+      const size = this.data.size
+      const query = wx.createSelectorQuery().in(this)
+      query.select('.swiper-wrapper').scrollOffset((res) => {
+        const scrollLeft = res.scrollLeft
+        let index = Math.ceil(scrollLeft / size)
+        if ((index * size - size / 2) < scrollLeft && (index * size + size / 2)) {
+
+        } else {
+          index--
+        }
+        this.setData({
+          i: index
+        }, () => {
+          this.selectMovie()
+        })
+      }).exec()
     }
+     */
   }
 })
