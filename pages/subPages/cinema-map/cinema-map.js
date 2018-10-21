6ccd2623a561e6bb20ff0362ace0125e8ed1a1e9
zhangzhihao <137596665@qq.com>
2018-10-21 18:21:19 +0800

    新增地图组件

--- pages/subPages/cinema-map/cinema-map.js
+++ pages/subPages/cinema-map/cinema-map.js
@@ -1,66 +1,23 @@
-// pages/subPages/cinema-map/cinema-map.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    info: null,
+    markers: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad(opt) {
+    this.initData({})
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  initData(info) {
+    this.setData({
+      info,
+      markers: [{
+        latitude: info.latitude || 30.498636,
+        longitude: info.longitude || 114.16837
+      }]
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //定位自己的位置
+  position() {
+    const MapContext = wx.createMapContext('map')
+    MapContext.moveToLocation()
   }
 })
